845a4083ecfd7c099e8c86ab1b5500fa26c2c5a6
alexlam184 <43229799+alexlam184@users.noreply.github.com>
2022-01-13 14:31:41 +0800

    fix: update husky,prettier,eslint

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en', 'hk'],
-    },
-  };
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'hk'],
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,32 @@
 import type { NextPage } from 'next';
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useTranslation } from 'next-i18next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useEffect } from 'react';
 
-
-
-const Home: NextPage  = () => {
-
-  const router = useRouter()
-  const { t } = useTranslation('home')
+const Home: NextPage = () => {
+  const router = useRouter();
+  const { t } = useTranslation('home');
 
   return (
     <>
       <main>
         <div>
-          <Link
-            href='/'
-            locale={router.locale === 'en' ? 'hk' : 'en'}
-          >
-            <button>
-              {t('change-locale')}
-            </button>
-
+          <Link href='/' locale={router.locale === 'en' ? 'hk' : 'en'}>
+            <button>{t('change-locale')}</button>
           </Link>
-         
         </div>
         {t('description')}
       </main>
     </>
-  )
-}
+  );
+};
 
-export const getStaticProps = async ({ locale }:any) => ({
-
+export const getStaticProps = async ({ locale }: any) => ({
   props: {
-    ...await serverSideTranslations(locale, ['header', 'home']),
+    ...(await serverSideTranslations(locale, ['header', 'home'])),
   },
-})
+});
 
-export default Home
+export default Home;
