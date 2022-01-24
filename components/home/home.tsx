28de11dac455d5892c68e4e61641af3be2aaa1d2
alexlam184 <43229799+alexlam184@users.noreply.github.com>
2022-01-24 14:56:34 +0800

    feat : personal description

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -5,15 +5,16 @@ import icon from '../../public/assets/photo/alex-icon.jpg';
 export const Home = () => {
   return (
     <div className='h-screen flex justify-center items-center'>
-      <div className=''>
-        <div className='inline-block'>
+      <div className='block bg-white border-2'>
+        <div className='flex justify-center items-center inline-block font-bold'>
           <span className='block'>Hi, My name is </span>
-          <span className='block'>Lam Alexander</span>
+          <span className='block bg-black text-white'>Lam Alexander</span>
         </div>
         <div className='inline-block bg-black w-[100px] h-[100px]'>
           <Image src={icon} alt='Picture of the author' />
         </div>
       </div>
+      <div className="block">© 2022 by Lam Alexander</div>
     </div>
   );
 };
