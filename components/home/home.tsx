7445c6cee11c49612e3c45213f13d39014b3bb55
alexlam184 <lamalexander1997@gmail.com>
2022-02-09 12:53:21 +0800

    fix: add var to css

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -7,21 +7,21 @@ export const Home = () => {
   const { t } = useTranslation('home');
 
   return (
-    <div className='relative h-screen bg-bkgroundblue flex'>
-      <div className='absolute object-center object-cover pointer-events-none w-1/2 right-0'>
+    <div className='relative h-screen bg-bkgroundblue flex justify-center items-center'>
+      <div className='xl:absolute md:relative object-center object-cover pointer-events-none md:w-[800px] xl:w-1/2 right-0'>
         <Image
           layout='responsive'
           src={computer}
           alt={'computer credit to:Guillaume Kurkdjian'}
         />
       </div>
-      <div className='absolute block left-10 top-20 z-1 bg-white border-8 bg-transparent'>
-        <div className='m-16'>
-          <span className='block text-white text-md md:text-2xl xl:text-4xl'>
+      <div className='absolute block left-10 xl:top-20 md:top-72 z-1 bg-white border-8 bg-transparent'>
+        <div className='md:my-16 xl:m-16'>
+          <span className='block text-white text-md md:text-6xl xl:text-4xl'>
             {t('greeting')}
           </span>
-          <span className='block bg-bkgrounddarkblue text-white mt-8 text-xl md:text-4xl xl:text-7xl'>
-            <strong className=' m-8'>{t('name')}</strong>
+          <span className='block bg-bkgrounddarkblue text-white mt-8 text-xl md:text-8xl xl:text-10xl'>
+            <strong className=''>{t('name')}</strong>
           </span>
           <ul className='option text-4xl'>
             <li>
